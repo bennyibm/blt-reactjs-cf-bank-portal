0a652de0fec04be91406e7011a6711f38c1ef868
Benny Nkonga <bennyibmn@gmail.com>
2022-02-11 15:10:31 +0100

    show header background-motif

--- src/component/header/header.tsx
+++ src/component/header/header.tsx
@@ -6,7 +6,7 @@ const Header : FunctionComponent = () =>{
     // variant of blue 2a2d7c6b
     // pure white fff
     return(
-        <div style={{backgroundColor : "#fff"}}>
+        <div>
             {/* <!-- fond-header--> */}
             <div id="fond-header"  className="fond-header pattern-header-01"></div>
 
